ef758bf132e6ea556760e6b7d9b3ae23a82e484b
Dimpal <vghldimple@gmail.com>
2024-12-02 18:00:23 +0530

    component add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Card } from './Component/Card';
 
 function App() {
   return (
     <div className="App">
-     <h1>hello </h1>
+    <Card/>
     </div>
   );
 }
