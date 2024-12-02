36efe958b42633818e1c968def7686f0deb64c55
krishna <vghldev02@gmail.com>
2024-12-02 21:54:57 +0530

    Login page making

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { Card } from './Component/Card';
+import { Login } from './Pages/Login';
 
 function App() {
   return (
-    <div className="App">
-    <div>
-    <Card/>
-    </div>
-    </div>
+   <>
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Login/>
+   </>
   );
 }
 
