30722f021f3a4922c2a0fa14f4141104013be46f
Dimpal <vghldimple@gmail.com>
2024-12-04 22:36:14 +0530

    forgetPAssword page making

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./Pages/Login"
 import { Dashbord } from "./Pages/Dashbord";
 import Register from "./Pages/Register";
+import ForgetPassword from "./Pages/ForgetPassword";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Register/>} />
           <Route path="login" element={<Login/>} />
+          <Route path="forgetpassword" element={<ForgetPassword/>} />
           <Route path="dashbord" element={<Dashbord/>} />
         </Routes>
       </BrowserRouter>
