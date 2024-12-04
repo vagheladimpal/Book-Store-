9a072ad150f6da521e0e6c708800e835b8515a97
krishna <vghldev02@gmail.com>
2024-12-04 22:33:29 +0530

    Landing page making

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes,Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Login from "./Pages/Login"
+import Login from "./Pages/Login";
 import { Dashbord } from "./Pages/Dashbord";
 import Register from "./Pages/Register";
+import Landing from "./Pages/Landing";
 
 function App() {
   return (
     <>
-  
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Register/>} />
-          <Route path="login" element={<Login/>} />
-          <Route path="dashbord" element={<Dashbord/>} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashbord" element={<Dashbord />} />
         </Routes>
       </BrowserRouter>
     </>
